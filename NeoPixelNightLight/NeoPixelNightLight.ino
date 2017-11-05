7808924273ffc9b71b6872004ea8b5b1c401e060
seekatar <seekatar@gmail.com>
2017-11-05 12:24:23 -0500

    add dst to nightlight

--- NeoPixelNightLight/NeoPixelNightLight.ino
+++ NeoPixelNightLight/NeoPixelNightLight.ino
@@ -46,10 +46,11 @@ Processor *processors[] = { new Backlight(current,wheel), new NeoClock(current, 
 
 #define MODE_SWITCH_PIN 10
 int mode = 0; 
+int inDst = 0;
 
 DateTime compileTime;
 
-const int MODE_INDEX = 0;  // EEPROM index
+const int MODE_INDEX = 0;           // EEPROM index of mode (int)
 
 /*******************************************************************************
 *******************************************************************************/
@@ -65,6 +66,7 @@ void setup() {
   pinMode(MODE_SWITCH_PIN, INPUT_PULLUP);
 
   EEPROM.get(MODE_INDEX, mode);
+  
   if ( mode < 0 || mode >= PROCESSOR_COUNT)
     mode = 0;
   Serial.print(F("Got mode from eeprom ") );
@@ -91,10 +93,28 @@ bool changingModes = false;
 // helper to get time for testing if no UTC available
 DateTime getCurrent()
 {
+  DateTime ret;
   if ( haveRtc )
-    return rtc.now();
+    ret = rtc.now();
   else
-    return compileTime.unixtime() + millis() / 50; // speed up time! use 1000 for close to real time
+    ret = compileTime.unixtime() + millis() / 50; // speed up time! use 1000 for close to real time
+
+  // need to sprint forward? 2am, second Sunday in March
+  if ( ret.month() == 2 && ret.dayOfTheWeek() == 0 && ret.hour() == 2 && (ret.day() % 7) == 1 )
+  {
+    inDst = 1;
+    ret = ret + TimeSpan(0,1,0,0);
+    rtc.adjust(ret);
+  }
+  else if ( ret.month() == 11 && ret.dayOfTheWeek() == 0 && ret.hour() == 2 && (ret.day() % 7) == 0 && inDst == 1 )
+  {
+    // need to fall back first Sunday in November 2am back to 1am
+    inDst = 0;
+    ret = ret - TimeSpan(0,1,0,0);
+    rtc.adjust(ret);
+  }
+  
+  return ret;
 }
 
 /*******************************************************************************
@@ -135,6 +155,55 @@ void loop() {
     pressed = false;
 }
 
+// from https://hackaday.com/2012/07/16/automatic-daylight-savings-time-compensation-for-your-clock-projects/
+/*--------------------------------------------------------------------------
+  FUNC: 6/11/11 - Returns day of week for any given date
+  PARAMS: year, month, date
+  RETURNS: day of week (0-7 is Sun-Sat)
+  NOTES: Sakamoto's Algorithm
+    http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week#Sakamoto.27s_algorithm
+    Altered to use char when possible to save microcontroller ram
+--------------------------------------------------------------------------*/
+char dow(int y, char m, char d)
+   {
+       static char t[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
+       y -= m < 3;
+       return (y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+   }
+ 
+/*--------------------------------------------------------------------------
+  FUNC: 6/11/11 - Returns the date for Nth day of month. For instance,
+    it will return the numeric date for the 2nd Sunday of April
+  PARAMS: year, month, day of week, Nth occurence of that day in that month
+  RETURNS: date
+  NOTES: There is no error checking for invalid inputs.
+--------------------------------------------------------------------------*/
+DateTime NthDayInMonth(int year, char month, char DOW, char NthWeek, int hour, int min ){
+  char targetDate = 1;
+  char firstDOW = dow(year,month,targetDate);
+  while (firstDOW != DOW){
+    firstDOW = (firstDOW+1)%7;
+    targetDate++;
+  }
+  //Adjust for weeks
+  targetDate += (NthWeek-1)*7;
+  return new DateTime ( year, month, targetDate, hour, min );
+}
+ 
+void setDst( DateTime current )
+{
+  // a bit imperfect if in the Fall back hour, but this only called when initialized, so don't flash it at those times.
+  
+  DateTime startDst = NthDayInMonth( current.year(), 3, 0, 2, 2, 0 ); // 2am second sunday in March
+  DateTime endDst = NthDayInMonth( current.year(), 11, 0, 1, 2, 0 ); // 2am first sunday in November
+  inDst = 1;
+  if ( current.secondstime() < startDst.secondstime() || 
+       current.secondstime() > endDst.secondstime() )
+       inDst = 0;
+  Serial.print("Setting DST to ");
+  Serial.println(inDst);
+}
+
 /*******************************************************************************
  * Setup the RealTimeClock breakout
 *******************************************************************************/
@@ -151,6 +220,7 @@ void rtcSetup() {
   if ( rtc.isrunning() ) {
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(compileTime);
+    setDst(compileTime);
   
     DateTime now = rtc.now();
     Serial.print( F("Rtc now is "));
